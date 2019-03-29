57db671a7ba67602d5a33b743e7ee95945f49664
Montse Badia <montse@digitalyou.es>
2019-03-29 18:58:10 +0100

    added cors configuration

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,13 @@ module Notify
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Allow CORS
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'localhost:3000'
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :patch, :delete, :options, :head]
+       end
+    end
   end
 end
